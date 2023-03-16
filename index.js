b210a502589a366f22996b9556519cb3fc55f429
Stephen <pena731.junk@gmail.com>
2023-03-16 18:21:43 -0400

    arrays lab 2

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis","Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+cats.unshift("Bob");
+
+}
+function destructivelyRemoveLastCat(){
+cats.pop();
+}
+function  destructivelyRemoveFirstCat(){
+cats.shift();
+
+}
+function appendCat(){
+    const copyofcats = [...cats.slice()];
+    copyofcats.push("Broom");
+     return copyofcats;
+
+}
+
+function prependCat(){
+    const copyofcats = [...cats.slice()];
+    copyofcats.unshift("Arnold");
+     return copyofcats;
+}
+
+function removeLastCat(){
+    const copyofcats = [...cats.slice()];
+    copyofcats.pop();
+     return copyofcats;
+}
+function removeFirstCat(){
+    const copyofcats = [...cats.slice()];
+    copyofcats.shift();
+     return copyofcats;
+}
